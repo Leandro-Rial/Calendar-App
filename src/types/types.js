e82e0f1134aed7da0c968e9dc3c59e9d30de280f
Leandro Rial <Leandro_Rial@outlook.com>
2021-08-20 16:23:26 -0300

    calendarApp without crud

---
+++ src/types/types.js
@@ -0,0 +1,17 @@
+export const types = {
+    uiOpenModal: '[UI] Open modal',
+    uiCloseModal: '[UI] Close modal',
+
+    eventSetActive: '[event] Set Active',
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+}
